bc1792ad340d71b3d668bf0110974f4466511de5
ElizPN <pankevych.liza@gmail.com>
2021-11-02 19:55:51 +0100

    test commit

--- getMaxSubSum.js
+++ getMaxSubSum.js
@@ -1,2 +1,18 @@
-let arr = [-1, 2, -4, 8]
-getMaxSubSum(arr) 
+
+
+
+let arr = [2, -1, 2, 3, -9];
+let arrSum = [];
+
+
+for (i = 0; i < arr.length; i++) {
+    let sum = 0;
+    sum = sum + arr[i];
+    arrSum.push(sum)
+
+  for ( j = arr[i] ; j < arr.length; j++) {
+  sum = sum + arr[j]
+  arrSum.push(sum)
+  } 
+
+}
