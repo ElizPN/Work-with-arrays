b0e8920153c308d201a3c578831aa3d97951daeb
ElizPN <pankevych.liza@gmail.com>
2021-11-10 11:41:55 +0100

    transform object array to name array

---
+++ transformToArrayName.js
@@ -0,0 +1,20 @@
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+function transformToArrayName (arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(arr[i].name) 
+        }
+    return newArr
+
+}
+
+let names = transformToArrayName(users);
+console.log(names);
+
+
+
