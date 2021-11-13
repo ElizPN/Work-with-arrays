501fa3dc051662219ecb03db51c98f293715ab45
ElizPN <pankevych.liza@gmail.com>
2021-11-13 18:09:04 +0100

    get unique elements in array

---
+++ unique_element.js
@@ -0,0 +1,22 @@
+let strings = [
+  "кришна",
+  "кришна",
+  "харе",
+  "харе",
+  "харе",
+  "харе",
+  "кришна",
+  "кришна",
+  ":-O",
+];
+function unique(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!newArr.includes(arr[i])) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+let uniqueElements = unique(strings);
+console.log(uniqueElements);
