d913afa39fe2480e0297e62c19a31313e3b941ef
ElizPN <pankevych.liza@gmail.com>
2022-02-05 23:16:58 +0200

    two way to reverse a string

---
+++ reverse_string.js
@@ -0,0 +1,9 @@
+let myName = "Yelyzaveta";
+
+let arrName = [];
+for (let i = myName.length - 1; i >= 0; i--) {
+  arrName.push(myName[i]);
+}
+console.log(arrName);
+console.log(arrName.toString());
+console.log(arrName.join());
